c3b8d1449530e1dd7857f10eff422c39d8800504
rijegaro287 <gatgens48@gmail.com>
2024-03-22 02:44:54 -0600

    Dependency handling

---
+++ proyecto1/compiler/tests/instructions.asm
@@ -0,0 +1,20 @@
+mov r1, #-10.73
+mov r2, #2.51
+
+add r3, r1, r2
+; sub r4, r1, r2
+; mul r5, r1, r2
+; div r6, r1, r2
+; mod r7, r1, r2
+; sll r8, r1
+
+; addv v1, v2, v3
+; subv v4, v2, v3
+; mulv v5, v2, v3
+; divv v6, v2, v3
+
+; str r1, r2
+; ldr r2, r3
+
+; strv v1, r2
+; ldrv v2, r3
---
+++ proyecto1/compiler/tests/stalls.asm
@@ -0,0 +1,10 @@
+mov r1, #-10.73
+mov r2, #2.51
+
+add r3, r1, r2
+add r2, r2, r3
+
+add r4, r5, r6
+add r5, r6, r7
+
+str r1, r5
